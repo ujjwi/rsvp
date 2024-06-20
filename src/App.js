1ff4c73d0d415495032941c439c91970968c0636
ujjwi <ujjwalr@iitbhilai.ac.in>
2024-06-20 17:35:09 +0530

    Initial react frontend setup and added navigation bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Profile from './components/Profile';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello!!</h1>
+      <>
+      <Router>
+        <Navbar />
+
+        {/* Using "exact path" instead of "path" becuase react does partial checking */}
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<Signup />} />
+          <Route exact path="/profile" element={<Profile />} />
+        </Routes>
+      </Router>
+      </>
     </div>
   );
 }
---
+++ src/components/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Profile() {
+  return (
+    <div>
+      User profile
+    </div>
+  )
+}
+
+export default Profile
